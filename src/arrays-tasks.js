cc565c6fdf6479aef508c1b652b6ad45fe8d46ec
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-02 11:00:39 +0300

    ffeat: add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -149,8 +149,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const { length } = arr[0];
+  return arr.every((item) => item.length === length);
 }
 
 /**
