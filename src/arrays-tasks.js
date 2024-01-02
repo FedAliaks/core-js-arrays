476a25f8fd7b4a14906fc58c69e1dc4340e47b96
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-02 11:55:34 +0300

    Feat: add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -290,8 +290,15 @@ function createNDimensionalArray(/* n, size */) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return [].concat(
+    ...nestedArray.map((item) => {
+      if (Array.isArray(item)) {
+        return flattenArray(item);
+      }
+      return item;
+    })
+  );
 }
 
 /**
